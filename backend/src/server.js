15faa8f98b80b79a934708ee09960ce55800f070
BazazAnas <BazazAnas@users.noreply.github.com>
2026-01-30 19:55:37 +0530

    resolved issues in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ env.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(cors({ origin : process.env.CLIENT_URL , credentials: true}))
 app.use(cookieParser());
 
