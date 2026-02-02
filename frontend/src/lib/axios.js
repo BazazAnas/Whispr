07821ff05b2a1174d6e787c12cfeb444d6fd8a45
BazazAnas <BazazAnas@users.noreply.github.com>
2026-02-02 17:17:45 +0530

    changes for deployment of frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:3000/api",
+    baseURL : "https://whispr-304m.onrender.com/api",
     withCredentials: true,
 })
